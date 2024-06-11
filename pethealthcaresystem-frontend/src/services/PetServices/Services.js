b446396d403ebbecfc6f6cd24e5c5bfa74d8a379
dathuynh2003 <vudat310103@gmail.com>
2024-06-11 22:51:41 +0700

    hot fix after merge 11/06

--- pethealthcaresystem-frontend/src/services/PetServices/Services.js
+++ pethealthcaresystem-frontend/src/services/PetServices/Services.js
@@ -61,16 +61,15 @@ export default function Services() {
       <h2 className='text-center mb-4 mt-4'><b>Our services</b></h2>
       <div className='row mx-auto mb-3'>
         {
-          services.map((service, index) => (
+          services?.map((service, index) => (
             <div key={index} className="col-md-3 mb-4 focus-ring"
-              onClick={() => { handleServiceClick(service.id, service.nameService, service.description, service.price, service.img) }}
+              onClick={() => { handleServiceClick(service?.id, service?.nameService, service?.description, service?.price, service?.img) }}
               style={{ cursor: 'pointer' }}
             >
               <div className="card shadow" style={{ width: "17rem" }} >
                 <img src={service?.img} className="card-img-top" style={{ width: "17rem", height: "10rem" }} alt="..." />
                 <div className="card-body" style={{ height: '100px' }}>
-                  <h5 className="card-title">{service.nameService}</h5>
-                  <p className="card-text">{service.description}</p>
+                  <p className="card-text">{service?.description}</p>
                 </div>
               </div>
             </div>
