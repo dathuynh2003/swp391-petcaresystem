489cc533d97768b4b159709e0f321e211e103d97
dathuynh2003 <vudat310103@gmail.com>
2024-06-14 01:51:09 +0700

    hot fix after update

--- pethealthcaresystem-backend/src/main/java/com/swpproject/pethealthcaresystem/controller/HospitalizationController.java
+++ pethealthcaresystem-backend/src/main/java/com/swpproject/pethealthcaresystem/controller/HospitalizationController.java
@@ -5,18 +5,37 @@ import com.swpproject.pethealthcaresystem.model.User;
 import com.swpproject.pethealthcaresystem.service.HospitalizationService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class HospitalizationController {
     @Autowired
     private HospitalizationService hospitalizationService;
+
+    @PostMapping("/admit/pet/{petId}")
+    public Map<String, Object> admitPet(@PathVariable int petId, HttpSession session) {
+        User curUser = (User) session.getAttribute("user");
+        Map<String, Object> response = new HashMap<>();
+        try {
+            if (curUser == null) {
+                throw new RuntimeException("You need to login first");
+            }
+            if (curUser.getRoleId() != 2 && curUser.getRoleId() != 3) {
+                throw new RuntimeException("You don't have permission to do this");
+            }
+            Hospitalization hospitalization = hospitalizationService.admitPet(petId, curUser.getUserId());
+            response.put("message", "Admitted pet successfully");
+            response.put("hospitalization", hospitalization);
+        } catch (RuntimeException e) {
+            response.put("message", e.getMessage());
+            response.put("hospitalization", null);
+        }
+        return response;
+    }
 
     @PostMapping("/admit/pet/{petId}/vet/{vetId}")
     public Map<String, Object> admitPet(@PathVariable int petId, @PathVariable int vetId, HttpSession session) {
--- pethealthcaresystem-frontend/src/services/ViewPet.js
+++ pethealthcaresystem-frontend/src/services/ViewPet.js
@@ -2,9 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 
 import axios from 'axios'
-import { TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
-import { Tab } from 'bootstrap';
-import { toast } from 'react-toastify';
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function ViewPet() {
 
@@ -46,7 +45,7 @@ export default function ViewPet() {
                 console.log(response.data.message)
             }
         } catch (e) {
-            toast.error("An unexpected error occurred")
+            toast.error(e.message)
             // console.error(e); // This helps to debug in case of an unexpected error
             setTimeout(() => {
                 navigate('/404page')
@@ -79,140 +78,147 @@ export default function ViewPet() {
 
     return (
         <div>
-            <div className='container'>
-                <div className='col-md-5 offset-md-4 border rounded-lg p-4 mt-2 shadow p-3 mb-5 bg-body-tertiary rounded'>
 
-                    <h2 className='text-center m-4'> Pet's Information</h2>
-                    <Tabs className="col-11 mt-3 mx-auto shadow p-3 mb-5 bg-body rounded h-100" colorScheme="teal" >
-                        <TabList className="d-flex justify-content-between">
-                            <Tab>Pet's Information</Tab>
-                            <Tab >Medical Record</Tab>
-                            <Tab >Hopitalization history</Tab>
-                        </TabList>
-                        <TabPanels>
-                            <TabPanel>
-                                <div className='container'>
-                                    <div className='col-md-6 mx-auto offset-md-4 border rounded-lg p-4 mt-2 shadow p-3 mb-5 bg-body-tertiary rounded'>
+            <Tabs className="col-11 mt-3 mx-auto shadow p-3 mb-5 bg-body rounded h-100" colorScheme="teal" >
+                <TabList className="d-flex justify-content-between">
+                    <Tab>Pet's Information</Tab>
+                    <Tab >Medical Record</Tab>
+                    <Tab >Hopitalization history</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                        <div className='container'>
+                            <div className='col-md-6 mx-auto offset-md-4 border rounded-lg p-4 mt-2 shadow p-3 mb-5 bg-body-tertiary rounded'>
 
-                                        <h2 className='text-center m-4'> Pet's Information</h2>
+                                <h2 className='text-center m-4'> Pet's Information</h2>
 
-                                        <div className='row row-cols-2'>
-                                            <div className="form-floating mb-3 col">
-                                                <input
-                                                    value={pet.name}
-                                                    type="text" className="form-control" id="name" readOnly />
-                                                <label htmlFor="name">Pet's name</label>
-                                            </div>
-                                            <div className="form-floating mb-3 col">
-                                                <input
-                                                    value={pet.petType}
-                                                    type="text" className="form-control" id="type" readOnly />
-                                                <label htmlFor="type">Pet's type</label>
-                                            </div>
-                                            <div className="form-floating mb-3 col">
-                                                <input
-                                                    value={pet.breed}
-                                                    type="text" className="form-control" id="breed" readOnly />
-                                                <label htmlFor="breed">Pet's breed</label>
-                                            </div>
-                                            <div className="form-floating mb-3 col">
-                                                <input
-                                                    value={pet.gender}
-                                                    type="text" className="form-control" id="gender" readOnly />
-                                                <label htmlFor="gender">Sex</label>
-                                            </div>
-
-
-                                            <div className="form-floating mb-3 col">
-                                                <input
-                                                    value={pet.isNeutered ? 'Yes' : 'No'}
-                                                    type="text" className="form-control" id="neutered" readOnly />
-                                                <label htmlFor="neutered">Neutered</label>
-                                            </div>
-
-                                            <div className="form-floating mb-3 col">
-                                                <input
-                                                    value={pet.age}
-                                                    type="text" className="form-control" id="age" readOnly />
-                                                <label htmlFor="age">Age (month(s))</label>
-                                            </div>
-                                        </div>
-                                        <div className="form-floating mb-3 col">
-                                            <input
-                                                value={pet.description}
-                                                type="text" className="form-control" id="description" readOnly />
-                                            <label htmlFor="description">Description</label>
-                                        </div>
-
-                                        {roleId !== '3' && (<Link className='btn btn-primary col-md-12' to="/listPets">Back</Link>)}
-                                        {roleId === '3' && (
-                                            pet?.hospitalizations?.some(admitPet => admitPet?.status === "admitted") ? (
-                                                <Link className='btn btn-danger col-md-12' onClick={() => handleDischarge(pet?.hospitalizations.find(hospitalization => hospitalization.status === "admitted").id)} >Discharge</Link>
-                                            ) : (
-                                                <Link className='btn btn-success col-md-12' onClick={() => handleHospitalize()} >Hospitalize</Link>
-                                            )
-                                        )}
+                                <div className='row row-cols-2'>
+                                    <div className="form-floating mb-3 col">
+                                        <input
+                                            value={pet.name}
+                                            type="text" className="form-control" id="name" readOnly />
+                                        <label htmlFor="name">Pet's name</label>
+                                    </div>
+                                    <div className="form-floating mb-3 col">
+                                        <input
+                                            value={pet.petType}
+                                            type="text" className="form-control" id="type" readOnly />
+                                        <label htmlFor="type">Pet's type</label>
+                                    </div>
+                                    <div className="form-floating mb-3 col">
+                                        <input
+                                            value={pet.breed}
+                                            type="text" className="form-control" id="breed" readOnly />
+                                        <label htmlFor="breed">Pet's breed</label>
+                                    </div>
+                                    <div className="form-floating mb-3 col">
+                                        <input
+                                            value={pet.gender}
+                                            type="text" className="form-control" id="gender" readOnly />
+                                        <label htmlFor="gender">Sex</label>
                                     </div>
 
 
+                                    <div className="form-floating mb-3 col">
+                                        <input
+                                            value={pet.isNeutered ? 'Yes' : 'No'}
+                                            type="text" className="form-control" id="neutered" readOnly />
+                                        <label htmlFor="neutered">Neutered</label>
+                                    </div>
+
+                                    <div className="form-floating mb-3 col">
+                                        <input
+                                            value={pet.age}
+                                            type="text" className="form-control" id="age" readOnly />
+                                        <label htmlFor="age">Age (month(s))</label>
+                                    </div>
                                 </div>
-                            </TabPanel>
-
-
-                            <TabPanel>
-                                <div className='container'>
-
+                                <div className="form-floating mb-3 col">
+                                    <input
+                                        value={pet.description}
+                                        type="text" className="form-control" id="description" readOnly />
+                                    <label htmlFor="description">Description</label>
                                 </div>
 
-                            </TabPanel>
+                                {roleId !== '3' && (<Link className='btn btn-primary col-md-12' to="/listPets">Back</Link>)}
+                                {roleId === '3' && (
+                                    pet?.hospitalizations?.some(admitPet => admitPet?.status === "admitted") ? (
+                                        <Link className='btn btn-danger col-md-12' onClick={() => handleDischarge(pet?.hospitalizations.find(hospitalization => hospitalization.status === "admitted").id)} >Discharge</Link>
+                                    ) : (
+                                        <Link className='btn btn-success col-md-12' onClick={() => handleHospitalize()} >Hospitalize</Link>
+                                    )
+                                )}
+                            </div>
 
 
-                            <TabPanel>
-
-                            </TabPanel>
-                        </TabPanels>
-
-                    </Tabs>
-                    <div>
-                    </div>
-                    <div className="form-floating mb-3 col">
-                        <input
-                            value={pet.breed}
-                            type="text" className="form-control" id="breed" readOnly />
-                        <label for="breed">Pet's breed</label>
-                    </div>
-                    <div className="form-floating mb-3 col">
-                        <input
-                            value={pet.gender}
-                            type="text" className="form-control" id="gender" readOnly />
-                        <label for="gender">Gender</label>
-                    </div>
+                        </div>
+                    </TabPanel>
 
 
-                    <div className="form-floating mb-3 col">
-                        <input
-                            value={pet.isNeutered ? 'Yes' : 'No'}
-                            type="text" className="form-control" id="neutered" readOnly />
-                        <label for="neutered">Neutered</label>
-                    </div>
+                    <TabPanel>
+                        <div className='container'>
 
-                    <div className="form-floating mb-3 col">
-                        <input
-                            value={pet.age}
-                            type="text" className="form-control" id="age" readOnly />
-                        <label for="age">Age</label>
-                    </div>
-                </div>
-                <div className="form-floating mb-3 col">
-                    <input
-                        value={pet.description}
-                        type="text" className="form-control" id="description" readOnly />
-                    <label for="description">Description</label>
-                </div>
+                        </div>
 
-                <Link className='btn btn-primary col-md-12' to="/listPets">Back</Link>
+                    </TabPanel>
 
+
+                    <TabPanel>
+
+                    </TabPanel>
+                </TabPanels>
+
+            </Tabs>
+            <div>
             </div>
-        </div >
+            <div className="form-floating mb-3 col">
+                <input
+                    value={pet.breed}
+                    type="text" className="form-control" id="breed" readOnly />
+                <label for="breed">Pet's breed</label>
+            </div>
+            <div className="form-floating mb-3 col">
+                <input
+                    value={pet.gender}
+                    type="text" className="form-control" id="gender" readOnly />
+                <label for="gender">Gender</label>
+            </div>
+
+
+            <div className="form-floating mb-3 col">
+                <input
+                    value={pet.isNeutered ? 'Yes' : 'No'}
+                    type="text" className="form-control" id="neutered" readOnly />
+                <label for="neutered">Neutered</label>
+            </div>
+
+            <div className="form-floating mb-3 col">
+                <input
+                    value={pet.age}
+                    type="text" className="form-control" id="age" readOnly />
+                <label for="age">Age</label>
+            </div>
+
+            <div className="form-floating mb-3 col">
+                <input
+                    value={pet.description}
+                    type="text" className="form-control" id="description" readOnly />
+                <label for="description">Description</label>
+            </div>
+
+            <Link className='btn btn-primary col-md-12' to="/listPets">Back</Link>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                // pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+        </div>
     )
 }
